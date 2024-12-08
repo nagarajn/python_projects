d71a37067640ebc001143db6e75dad8e98b56908
Nagaraj N <nagarajatmsrit@gmail.com>
2024-12-09 00:45:25 +0530

    First

---
+++ first/first.py
@@ -0,0 +1,8 @@
+import math
+
+
+def calc_sum(x, y):
+    return x + y
+
+print(calc_sum(10,20))
+print (calc_sum(10,20))
---
+++ first/test_first.py
@@ -0,0 +1,5 @@
+import first
+import pytest
+
+def test_calc_sum():
+    assert first.calc_sum(10,20) == 30
