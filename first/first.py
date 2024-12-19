4a9735f59fbd3b5e08efe6eac5f86c939752431e
Nagaraj N <nagarajatmsrit@gmail.com>
2024-12-19 14:24:33 +0530

    Pandas update

--- first/first.py
+++ first/first.py
@@ -1,8 +1,14 @@
 import math
-
-
+import pandas as pd
+import matplotlib.pyplot as plt
 def calc_sum(x, y):
     return x + y
 
 print(calc_sum(10,20))
 print (calc_sum(10,20))
+
+df = pd.read_csv("titanic.csv")
+print(df.describe())
+
+df.plot()
+plt.show()
